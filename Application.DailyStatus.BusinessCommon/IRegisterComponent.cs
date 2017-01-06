53527e6ba6387b3b61b10095a4777f1d51264f13
Jigar Bagadai <jbagadai@insta>
2017-01-06 17:23:58 +0530

    MEF Inprogress

---
+++ Application.DailyStatus.BusinessCommon/IRegisterComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DailyStatus.BusinessCommon
+{
+    public  interface IRegisterComponent
+    {
+        void RegisterType<TFrom, TTo>(bool withInterception = false) where TTo : TFrom;
+
+        void RegisterTypeWithControlledLifeTime<TFrom, TTo>(bool withInterception = false) where TTo : TFrom;
+    }
+}
---
+++ Application.DailyStatus.BusinessCommon/RegisterComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DailyStatus.BusinessCommon
+{
+    internal class RegisterComponent : IRegisterComponent
+    {
+        private readonly IUnityContainer _container;
+
+        public RegisterComponent(IUnityContainer container)
+        {
+            this._container = container;
+        }
+
+        public void RegisterType<TFrom, TTo>(bool withInterception = false) where TTo : TFrom
+        {
+            if (withInterception)
+            {
+                //register with interception
+            }
+            else
+            {
+                this._container.RegisterType<TFrom, TTo>();
+            }
+        }
+
+        public void RegisterTypeWithControlledLifeTime<TFrom, TTo>(bool withInterception = false) where TTo : TFrom
+        {
+            this._container.RegisterType<TFrom, TTo>(new ContainerControlledLifetimeManager());
+        }
+    }
+}
