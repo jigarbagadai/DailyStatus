77c36de2f81cfd8e747b9e92616c3ec0cb3b9719
Jigar Bagadai <jbagadai@insta>
2017-01-13 17:27:23 +0530

    Logger

---
+++ Application.DailyStatus.Logging/ILogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DailyStatus.Logging
+{
+    public interface ILogger
+    {
+        void Debug(string format, params object[] args);
+        void Exception(string format, params object[] args);
+        void Exception(Exception ex);
+        void Fatal(string format, params object[] args);
+        void Info(string format, params object[] args);
+        void Trace(string format, params object[] args);
+        void Warn(string format, params object[] args);
+    }
+}
---
+++ Application.DailyStatus.Logging/NLogger.cs
@@ -0,0 +1,54 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DailyStatus.Logging
+{
+    public class NLogger : ILogger
+    {
+        private readonly Logger logger;
+
+        public NLogger()
+        {
+            logger = LogManager.GetLogger("applicationLogger");
+        }
+
+        public void Debug(string format, params object[] args)
+        {
+            this.logger.Debug(format, args);
+        }
+
+        public void Exception(Exception ex)
+        {
+            this.logger.Error(ex);
+        }
+
+        public void Exception(string format, params object[] args)
+        {
+            this.logger.Error(format, args);
+        }
+
+        public void Fatal(string format, params object[] args)
+        {
+            this.logger.Fatal(format, args);
+        }
+
+        public void Info(string format, params object[] args)
+        {
+            this.logger.Info(format, args);
+        }
+
+        public void Trace(string format, params object[] args)
+        {
+            this.logger.Trace(format, args);
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            this.logger.Warn(format, args);
+        }
+    }
+}
